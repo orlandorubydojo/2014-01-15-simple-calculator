fb1a2c19d77213b9d473ee33b22a066d1eaf6425
Trace Helms <tracehelms@gmail.com>
2014-01-15 21:52:03 -0500

    Code from Dojo

--- simple_calculator.rb
+++ simple_calculator.rb
@@ -1,3 +1,22 @@
 class SimpleCalculator
-  # TODO Implement this!
+  def add(value1, value2)
+    # raise UnexpectedMathError if value1.is_a?(String)
+    value1 + value2
+  end
+
+  def subtract(value1, value2)
+    value1 - value2
+  end
+
+  def multiply(value1, value2)
+    value1 * value2
+  end
+  def divide(value1, value2)
+    return 0 if value2 == 0
+    value1 / value2
+  end
+end
+
+class UnexpectedMathError
+
 end
--- simple_calculator_spec.rb
+++ simple_calculator_spec.rb
@@ -1,3 +1,75 @@
 require './simple_calculator.rb'
 
 # TODO Write some tests!
+
+describe SimpleCalculator do
+  before do
+    @calculator = SimpleCalculator.new
+  end
+
+  context "adding" do
+    it "should add two numbers" do
+      result = @calculator.add(1, 2)
+      expect(result).to eq 3
+    end
+
+    it "should handle adding zero" do
+      result = @calculator.add(3, 0)
+      expect(result).to eq 3
+    end
+
+    it "should raise an error if trying to add a string" do
+      expect{ @calculator.add("r", 5) }.to raise_error(UnexpectedMathError)
+    end
+  end
+
+  context "subtracting" do
+    it "should subtract two numbers" do
+      result = @calculator.subtract(4, 2)
+      expect(result).to eq 2
+    end
+
+    it "should be able to return a negative number" do
+      result = @calculator.subtract(2, 4)
+      expect(result).to eq -2
+    end
+  end
+
+  context "multiplication" do
+    it "should multiply two numbers" do
+      result = @calculator.multiply(2, 3)
+      expect(result).to eq 6
+    end
+
+    it "should return zero" do
+      result = @calculator.multiply(4, 0)
+      expect(result).to eq 0
+    end
+
+  end
+
+  context "division" do
+    it "should divide two numbers" do
+      result = @calculator.divide(6, 2)
+      expect(result).to eq 3
+    end
+
+    it "should return zero" do
+      result = @calculator.divide(4, 0)
+      expect(result).to eq 0
+    end
+
+    it "should return negative number" do
+      result = @calculator.divide(6, -2)
+      expect(result).to eq -3
+    end
+
+    it "should return positive when getting two negatives" do
+      result = @calculator.divide(-6,-3)
+      expect(result).to eq 2
+    end
+
+  end
+
+
+end
